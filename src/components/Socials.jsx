6a8cb13bfc1427341e09f203d7d0ce7413606fd1
Vertori <115083330+Vertori@users.noreply.github.com>
2023-05-26 12:07:05 +0200

    socials component

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 const Socials = () => {
-  return <div className="hidden xl:flex">Socials</div>;
+  return (
+    <div className="hidden xl:flex ml-24">
+      <ul className="flex gap-x-4">
+        <li>
+          <a href="https://linkedin.com/" target="_blank" className="text-xl">
+            <BsLinkedin />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/" target="_blank" className="text-xl">
+            <BsGithub />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Socials;
